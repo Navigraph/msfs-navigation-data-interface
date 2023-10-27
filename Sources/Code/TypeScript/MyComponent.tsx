006b7bf4d91246350d86a972eb56bcaebc93628c
Malte Hallström <malte.hallstrom@gmail.com>
2023-10-27 14:10:02 +0200

    chore: remove unused file

--- Sources/Code/TypeScript/MyComponent.tsx
+++
@@ -1,7 +0,0 @@
-import { DisplayComponent, FSComponent, VNode } from "@microsoft/msfs-sdk"
-
-export class MyComponent extends DisplayComponent<any> {
-  public render(): VNode {
-    return <div class="my-component">Hello World!</div>
-  }
-}
