a9a7f772020d2b01bceeee093e985b7855f5336a
Malte Hallström <malte.hallstrom@gmail.com>
2024-01-29 17:02:56 +0100

    ci: revert to incorrect (but working) script
    This configuration seems to result in successful test runs for some reason ¯\_(ツ)_/¯

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,4 @@
 rm -rf test_work
 mkdir test_work
 
-cd "$(dirname "$0")"
-
-./run_docker_cmd.sh npm ci
-./run_docker_cmd.sh npm run jest
+source "${BASH_SOURCE%/*}/run_docker_cmd.sh" npm ci && npm run jest
