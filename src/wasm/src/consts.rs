312d3e1aca4db3da9cfe1dbef597f89f13ca662c
Jack Lavigne <jacklavigne00@gmail.com>
2024-05-13 16:31:41 -0400

    chore: rename internal config

--- src/wasm/src/consts.rs
+++ src/wasm/src/consts.rs
@@ -1,3 +1,3 @@
 pub const NAVIGATION_DATA_DEFAULT_LOCATION: &str = ".\\NavigationData";
 pub const NAVIGATION_DATA_WORK_LOCATION: &str = "\\work/NavigationData";
-pub const NAVIGATION_DATA_INTERNAL_CONFIG_LOCATION: &str = "\\work/navigraph_navigation_data_interface_config.json";
+pub const NAVIGATION_DATA_INTERNAL_CONFIG_LOCATION: &str = "\\work/navigraph_config.json";
