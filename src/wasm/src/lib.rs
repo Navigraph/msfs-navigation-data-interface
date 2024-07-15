3267edb6aee78f01a2ae2a1e14fa5666af65311a
Jack Lavigne <jacklavigne00@gmail.com>
2024-07-15 19:41:59 -0400

    feat: add version logging to wasm

--- src/wasm/src/lib.rs
+++ src/wasm/src/lib.rs
@@ -8,6 +8,14 @@ mod util;
 
 #[msfs::gauge(name=navigation_data_interface)]
 async fn navigation_data_interface(mut gauge: msfs::Gauge) -> Result<(), Box<dyn std::error::Error>> {
+    // Log the current version of the module
+    println!(
+        "{}",
+        format!(
+            "[NAVIGRAPH]: Navigation data interface version {} started",
+            env!("CARGO_PKG_VERSION")
+        )
+    );
     let mut dispatcher = dispatcher::Dispatcher::new();
     while let Some(event) = gauge.next_event().await {
         dispatcher.on_msfs_event(event);
