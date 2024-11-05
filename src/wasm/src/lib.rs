1eb76c8bcfe89d552b3f7a9da9c904acebe18c49
Falcon <falcon@zigtag.dev>
2024-11-04 17:49:13 -0800

    feat: version now shows short git hash as well

--- src/wasm/src/lib.rs
+++ src/wasm/src/lib.rs
@@ -6,10 +6,13 @@ mod util;
 
 #[msfs::gauge(name=navigation_data_interface)]
 async fn navigation_data_interface(mut gauge: msfs::Gauge) -> Result<(), Box<dyn std::error::Error>> {
+    let hash = env!("GIT_HASH").split_at(7).0;
+
     // Log the current version of the module
     println!(
-        "[NAVIGRAPH]: Navigation data interface version {} started",
-        env!("CARGO_PKG_VERSION")
+        "[NAVIGRAPH]: Navigation data interface version {}-{} started",
+        env!("CARGO_PKG_VERSION"),
+        hash
     );
     let mut dispatcher: dispatcher::Dispatcher<'_> =
         dispatcher::Dispatcher::new(navigation_database::enums::InterfaceFormat::DFDv2);
