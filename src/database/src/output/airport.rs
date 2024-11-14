b0b1b4f9c263cdf40ef94d0421584a5f0beda74e
Falcon <falcon@zigtag.dev>
2024-11-13 16:19:21 -0800

    chore: cargo fmt

--- src/database/src/output/airport.rs
+++ src/database/src/output/airport.rs
@@ -3,7 +3,8 @@ use serde::Serialize;
 use crate::{
     enums::{IfrCapability, RunwaySurfaceCode},
     math::{Coordinates, Degrees, Feet},
-    sql_structs, v2,
+    sql_structs,
+    v2,
 };
 
 #[serde_with::skip_serializing_none]
