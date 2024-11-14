628e97599a40f2661a7fb7cb42e983224c122343
Falcon <falcon@zigtag.dev>
2024-11-13 16:55:07 -0800

    fix: workflow script 2

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,5 +4,5 @@ rm -rf test_work
 mkdir test_work
 mkdir test_work/navigraph-test
 
-source "${BASH_SOURCE%/*}/run_docker_cmd.sh" npm ci
-source "${BASH_SOURCE%/*}/run_docker_cmd.sh" npm run jest
+bash ./scripts/run_docker_cmd.sh npm ci
+bash ./scripts/run_docker_cmd.sh npm run jest
