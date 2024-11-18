42db948ec769c15d1fa620b3b1238feaeb87908e
Jack Lavigne <jacklavigne00@gmail.com>
2024-11-18 10:37:07 +0100

    chore: remove `build_dev` script

--- scripts/build_dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Flags needed to get sqlite3 to work in the sim
-export LIBSQLITE3_FLAGS="-DSQLITE_OMIT_SHARED_CACHE -D_LARGEFILE64_SOURCE"
-
-cargo build --target wasm32-wasi
-
-cp /external/target/wasm32-wasi/debug/msfs_navigation_data_interface.wasm /out/msfs_navigation_data_interface.wasm 
