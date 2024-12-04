9270ca308b777d7d146b7cf7771cca8b9fdf9831
Falcon <falcon@zigtag.dev>
2024-12-04 15:23:05 +0100

    feat: now builds with cargo-msfs

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,7 +2,8 @@
 
 cd %~dp0
 
-call .\run_docker_cmd.bat ./scripts/build.sh
+call cargo-msfs build msfs%1
+call wasm-opt -O1 --signext-lowering --enable-bulk-memory -o ../out/msfs_navigation_data_interface.wasm ../target/wasm32-wasip1/release/msfs_navigation_data_interface.wasm
 
 cd %~dp0
 
