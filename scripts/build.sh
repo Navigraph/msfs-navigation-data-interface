5330b74e0a0990d1b11bebbe505620ac6598049c
Falcon <falcon@zigtag.dev>
2025-02-06 16:50:57 -0800

    fix: trying another name

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,13 @@
-mkdir ./out20
-mkdir ./out24
+mkdir out20
+mkdir out24
 
 echo Installing MSFS SDK
 
-# cargo-msfs install msfs2020
-# cargo-msfs install msfs2024
+cargo-msfs install msfs2020
+cargo-msfs install msfs2024
 
 echo Building packages in dir $(pwd)
-ls
 
-cargo-msfs build msfs2020 -i . -o ./out20/msfs_navigation_data_interface.wasm
-cargo-msfs build msfs2024 -i . -o ./out24/msfs_navigation_data_interface.wasm
+cargo-msfs build msfs2020 -i . -o out20/msfs_navigation_data_interface.wasm
+cargo-msfs build msfs2024 -i . -o out24/msfs_navigation_data_interface.wasm
 
