1a67216cd7632b69ada685af1a47be11a2046a1c
Falcon <falcon@zigtag.dev>
2025-02-06 16:37:04 -0800

    chore: add pwd to see how its having an issue

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,10 @@ mkdir ./out24
 
 echo Installing MSFS SDK
 
-cargo-msfs install msfs2020
-cargo-msfs install msfs2024
+# cargo-msfs install msfs2020
+# cargo-msfs install msfs2024
 
-echo Building packages
+echo Building packages in dir $(pwd)
 
 cargo-msfs build msfs2020 -i . -o ./out20/msfs_navigation_data_interface.wasm
 cargo-msfs build msfs2024 -i . -o ./out24/msfs_navigation_data_interface.wasm
