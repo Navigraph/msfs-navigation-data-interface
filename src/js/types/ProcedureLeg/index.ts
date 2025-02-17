3cc3447c74d2f1e206c6663b4d1272b040f74614
Michael Romashov <mromashov@icloud.com>
2025-02-17 15:56:36 -0500

    add `FXLegData` to `ProcedureLeg` union

--- src/js/types/ProcedureLeg/index.ts
+++ src/js/types/ProcedureLeg/index.ts
@@ -149,7 +149,7 @@ export interface ProcedureLegBase {
 export type HXLegData = HALegData | HFLegData | HMLegData
 export type XFLegData = AFLegData | CFLegData | DFLegData | IFLegData | RFLegData | TFLegData | HXLegData
 export type FXLegData = FALegData | FMLegData | FDLegData
-export type ProcedureLeg = XFLegData | CALegData | XILegData | XDLegData | VALegData | VMLegData | XRLegData
+export type ProcedureLeg = XFLegData | FXLegData | CALegData | XILegData | XDLegData | VALegData | VMLegData | XRLegData
 
 export * from "./AFLeg"
 export * from "./CALeg"
