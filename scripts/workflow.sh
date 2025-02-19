5bf4efc0a8e06359a322858faa3e1ddc3758297e
Falcon <falcon@zigtag.dev>
2025-02-19 15:57:37 -0800

    fix: more flags

--- scripts/workflow.sh
+++ scripts/workflow.sh
@@ -4,5 +4,7 @@ docker build -t local .
 
 ./scripts/run_docker_cmd.sh ./scripts/build.sh both
 
+echo "Building done, now zipping"
+
 zip -j interface2020.zip out20/*
 zip -j interface2024.zip out24/*
