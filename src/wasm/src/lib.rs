9fe50393337add10f1689319d37e86fc45fe625c
Falcon <falcon@zigtag.dev>
2025-02-27 16:41:07 -0800

    wip: implement sentry

--- src/wasm/src/lib.rs
+++ src/wasm/src/lib.rs
@@ -1,4 +1,7 @@
-use std::env;
+use std::{env, sync::Arc, time::Instant};
+
+use dotenvy_macro::dotenv;
+use sentry_util::SentryTransportFactory;
 
 mod consts;
 mod dispatcher;
@@ -6,7 +9,10 @@ mod download;
 mod json_structs;
 mod meta;
 mod network_helper;
+mod sentry_util;
 mod util;
+
+const SENTRY_URL: &str = dotenv!("SENTRY_URL");
 
 #[msfs::gauge(name=navigation_data_interface)]
 async fn navigation_data_interface(
@@ -23,15 +29,33 @@ async fn navigation_data_interface(
         );
     }
 
+    let release_name = format!("{}-{}", env!("CARGO_PKG_VERSION"), hash);
+
     // Log the current version of the module
     println!(
-        "[NAVIGRAPH]: Navigation data interface version {}-{} started",
-        env!("CARGO_PKG_VERSION"),
-        hash
+        "[NAVIGRAPH]: Navigation data interface version {} started",
+        &release_name
     );
+
+    let guard = sentry::init((
+        SENTRY_URL,
+        sentry::ClientOptions {
+            release: Some(release_name.into()),
+            transport: Some(Arc::new(SentryTransportFactory)),
+            ..Default::default()
+        },
+    ));
+
+    // sentry::capture_message("test message", sentry::Level::Fatal);
+
     let mut dispatcher = dispatcher::Dispatcher::new();
+    let mut last_flush = Instant::now();
     while let Some(event) = gauge.next_event().await {
         dispatcher.on_msfs_event(event);
+        if last_flush.elapsed().as_secs_f64() <= 30. {
+            guard.flush(None);
+            last_flush = Instant::now();
+        }
     }
 
     Ok(())
---
+++ src/wasm/src/sentry_util.rs
@@ -0,0 +1,64 @@
+use std::{
+    rc::Rc,
+    sync::{Arc, RwLock},
+};
+
+use msfs::network::NetworkRequestBuilder;
+use sentry::{ClientOptions, Transport, TransportFactory};
+
+struct CustomSentryTransport {
+    options: ClientOptions,
+}
+
+impl Transport for CustomSentryTransport {
+    fn send_envelope(&self, envelope: sentry::Envelope) {
+        // println!("[NAVIGRAPH]: Envelope request received {:?}", envelope);
+        let mut body = Vec::new();
+        envelope.to_writer(&mut body).unwrap();
+
+        let dsn = self.options.dsn.as_ref().unwrap();
+        let user_agent = self.options.user_agent.clone();
+        let auth = dsn.to_auth(Some(&user_agent)).to_string();
+        let url = dsn.envelope_api_url().to_string();
+
+        let callback_received = Rc::new(RwLock::new(false));
+
+        let cloned = Rc::clone(&callback_received);
+        if let Some(res) = NetworkRequestBuilder::new(&url)
+            .unwrap()
+            .with_header(&format!("X-Sentry-Auth: {}", auth))
+            .unwrap()
+            .with_data(&mut body.clone())
+            .with_callback(move |_e, _s| {
+                let mut writer = cloned.write().unwrap();
+                *writer = true;
+                println!("[NAVIGRAPH]: callback!");
+            })
+            .post(&String::from_utf8(body).unwrap())
+        {
+            let ec = res.error_code();
+
+            println!("[NAVIGRAPH]: Res Code: {}", ec);
+
+            if ec == 0 {
+                println!("{:?}", res.data())
+            } else {
+                println!("[NAVIGRAPH]: Posted to Sentry");
+            }
+        } else {
+            println!("[NAVIGRAPH]: Sentry failed to get res");
+        };
+
+        // while !(*callback_received.read().unwrap()) {}
+    }
+}
+
+pub struct SentryTransportFactory;
+
+impl TransportFactory for SentryTransportFactory {
+    fn create_transport(&self, options: &sentry::ClientOptions) -> Arc<dyn sentry::Transport> {
+        Arc::new(CustomSentryTransport {
+            options: options.clone(),
+        })
+    }
+}
