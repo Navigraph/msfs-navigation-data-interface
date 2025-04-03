8d83c4f6a5e6da6057b4c94d348cfc715122276e
Jack Lavigne <jacklavigne00@gmail.com>
2025-04-03 20:43:39 +0200

    refactor: clean up repo a bit
    - `examples` -> `example`
    - remove all root level js-related files for now before putting bun in
    - remove scripts
    - remove `src/test` (for now)

--- scripts/run_docker_cmd.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set image="local"
-
-cd %~dp0
-
-docker run --rm -it -v "%cd%\..\:/external" -w /external %image% %*
--- scripts/test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd %~dp0
-
-rmdir /s /q ..\test_work
-mkdir ..\test_work
-
-:: Docker is a must for now, WASI preopens don't exist on windows.
-call .\run_docker_cmd.bat npm run jest
