22fdb94336992c026b5fd668ce9767533b6a4c03
Malte Hallström <malte.hallstrom@gmail.com>
2025-04-07 18:12:36 +0200

    chore: use parseArgs

--- scripts/cargo-msfs.ts
+++ scripts/cargo-msfs.ts
@@ -1,6 +1,7 @@
 import { $ } from "bun";
-import { existsSync, mkdirSync, rmdirSync, readFileSync } from "node:fs";
+import { existsSync, mkdirSync, readFileSync, rmdirSync } from "node:fs";
 import { dirname, join, normalize, resolve } from "node:path";
+import { parseArgs } from "util";
 
 /// The type returned from the `cargo-msfs info -f` command
 interface InstalledSdkVersions {
@@ -36,76 +37,74 @@ function findWorkspaceRoot() {
   return null;
 }
 
-async function main() {
-  // Get workspace root for docker commands
-  const workspaceRoot = findWorkspaceRoot();
-  if (!workspaceRoot) {
-    console.error("[-] Unable to find workspace root. Exiting...");
-    process.exit(1);
-  }
+// Determine which version(s) to build based on command line argument --version
+const allowedVersions = ["msfs2020", "msfs2024"];
 
-  // Ensure docker is installed and available
-  await $`docker ps`.quiet().catch(() => {
-    console.error("[-] Docker is not installed or not running");
-    process.exit(1);
-  });
+const { values } = parseArgs({
+  args: Bun.argv,
+  options: { version: { type: "string" } },
+  strict: true,
+  allowPositionals: true,
+});
 
-  // Ensure image is built
-  await $`docker image inspect ${IMAGE_NAME}:latest`.quiet().catch(async () => {
-    const dockerfilePath = resolve(workspaceRoot, "Dockerfile");
-    console.info(`[*] Building '${IMAGE_NAME}' image from ${dockerfilePath}`);
-    await $`docker build -t ${IMAGE_NAME} -f ${dockerfilePath} .`;
-  });
+if (values.version && !allowedVersions.includes(values.version)) {
+  console.error(`Invalid version argument: ${values.version}. Allowed values are ${allowedVersions.join(", ")}`);
+  process.exit(1);
+}
 
-  // Ensure SDKs are up to date, rebuilding if needed
-  const installedSdks = JSON.parse(
+const versionsToBuild = values.version ? [values.version] : allowedVersions;
+
+// Get workspace root for docker commands
+const workspaceRoot = findWorkspaceRoot();
+if (!workspaceRoot) {
+  console.error("[-] Unable to find workspace root. Exiting...");
+  process.exit(1);
+}
+
+// Ensure docker is installed and available
+await $`docker ps`.quiet().catch(() => {
+  console.error("[-] Docker is not installed or not running");
+  process.exit(1);
+});
+
+// Ensure image is built
+await $`docker image inspect ${IMAGE_NAME}:latest`.quiet().catch(async () => {
+  const dockerfilePath = resolve(workspaceRoot, "Dockerfile");
+  console.info(`[*] Building '${IMAGE_NAME}' image from ${dockerfilePath}`);
+  await $`docker build -t ${IMAGE_NAME} -f ${dockerfilePath} .`;
+});
+
+// Ensure SDKs are up to date, rebuilding if needed
+const installedSdks = JSON.parse(
     await $`docker run --rm ${IMAGE_NAME} bash -c "cargo-msfs info -f"`.text(),
-  ) as InstalledSdkVersions;
-  if (installedSdks.versions.some(v => !v.up_to_date)) {
-    console.info("[*] Updating SDK in Docker image...");
-    await $`docker build --build-arg CACHEBUST=${Date.now()} -t ${IMAGE_NAME} -f ${resolve(workspaceRoot, "Dockerfile")} .`;
-  }
+) as InstalledSdkVersions;
+if (installedSdks.versions.some(v => !v.up_to_date)) {
+  console.info("[*] Updating SDK in Docker image...");
+  await $`docker build --build-arg CACHEBUST=${Date.now()} -t ${IMAGE_NAME} -f ${resolve(workspaceRoot, "Dockerfile")} .`;
+}
 
-  // Clear out dir
-  const outDir = resolve(workspaceRoot, "dist/wasm");
-  if (existsSync(outDir)) rmdirSync(outDir, { recursive: true });
+// Clear out dir
+const outDir = resolve(workspaceRoot, "dist/wasm");
+if (existsSync(outDir)) rmdirSync(outDir, { recursive: true });
 
-  // The work directory, relative to workspace root
-  const relativeWorkdDir = process.cwd().replace(workspaceRoot, "").replaceAll("\\", "/");
+// The work directory, relative to workspace root
+const relativeWorkdDir = process.cwd().replace(workspaceRoot, "").replaceAll("\\", "/");
 
-  // Determine which version(s) to build based on command line argument --version
-  const allowedVersions = ["msfs2020", "msfs2024"];
-  let versionsToBuild = allowedVersions;
+// Build the selected versions
+for (const version of versionsToBuild) {
+  console.info(`[*] Building for ${version}`);
 
-  const versionArgIndex = process.argv.indexOf("--version");
-  if (versionArgIndex !== -1 && process.argv[versionArgIndex + 1]) {
-    const versionArg = process.argv[versionArgIndex + 1];
-    if (versionArg && allowedVersions.includes(versionArg)) {
-      versionsToBuild = [versionArg];
-    } else {
-      console.error(`Invalid version argument: ${versionArg}. Allowed values are ${allowedVersions.join(", ")}`);
-      process.exit(1);
-    }
-  }
+  // Create the subfolder
+  const simDir = join(outDir, version);
+  const relativeSimDir = simDir.replace(workspaceRoot, "").replaceAll("\\", "/");
+  mkdirSync(simDir, { recursive: true });
 
-  // Build the selected versions
-  for (const version of versionsToBuild) {
-    console.info(`[*] Building for ${version}`);
-
-    // Create the subfolder
-    const simDir = join(outDir, version);
-    const relativeSimDir = simDir.replace(workspaceRoot, "").replaceAll("\\", "/");
-    mkdirSync(simDir, { recursive: true });
-
-    // Run cargo-msfs
-    await $`docker run \
+  // Run cargo-msfs
+  await $`docker run \
     -v ${workspaceRoot}:/workspace \
     -w /workspace${relativeWorkdDir} \
     ${IMAGE_NAME} \
     bash -c "cargo-msfs build ${version} -i ./src/wasm -o ./${relativeSimDir}/msfs_navigation_data_interface.wasm"`.catch(
-      (err: { exitCode?: number }) => process.exit(err.exitCode ?? 1),
-    );
-  }
+    (err: { exitCode?: number }) => process.exit(err.exitCode ?? 1),
+  );
 }
-
-await main();
